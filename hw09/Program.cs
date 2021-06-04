85bf516e245c550d13baedb59247a9f0337dd676
ArtemIyX <68477399+ArtemIyX@users.noreply.github.com>
2021-06-04 20:26:14 +0300

    Добавьте файлы проекта.

---
+++ hw09/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace hw09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LibraryContext context = new LibraryContext())
+            {
+                var list = context.Books.Include(u => u.Author).ToList();
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item.Title} ({item.Author.Name})");
+                }
+            }
+        }
+    }
+}
---
+++ hw09/SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE Library;
+USE Library;
+CREATE TABLE [dbo].[Author] (
+    [id]   INT           IDENTITY (1, 1) NOT NULL,
+    [name] NVARCHAR (50) NULL,
+    CONSTRAINT [Author_pk] PRIMARY KEY NONCLUSTERED ([id] ASC)
+);
+CREATE TABLE [dbo].[Book] (
+    [id]        INT            IDENTITY (1, 1) NOT NULL,
+    [title]     NVARCHAR (150) NULL,
+    [cost]      INT            NULL,
+    [amount]    INT            NULL,
+    [author_id] INT            NULL,
+    CONSTRAINT [Book_pk] PRIMARY KEY NONCLUSTERED ([id] ASC),
+    CONSTRAINT [Book_Author_id_fk] FOREIGN KEY ([author_id]) REFERENCES [dbo].[Author] ([id])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [Book_title_uindex]
+    ON [dbo].[Book]([title] ASC);
+
